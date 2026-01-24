e459abca0b3c8dbf0da47947b0045726f4e81e78
Komaleswar <n210038@rguktn.ac.in>
2026-01-24 12:46:17 +0530

    Remove all files from main branch

--- apps/frontend/components/PortfolioCard.js
+++
@@ -1,9 +0,0 @@
-export default function PortfolioCard({title, value}){
-  return (
-    <div className="bg-white p-4 rounded shadow">
-      <div className="text-sm text-gray-500">{title}</div>
-      <div className="text-2xl font-semibold">{value}</div>
-    </div>
-  )
-}
-
